675e22329a93ea424eed8cbf770120e65e7f33b6
PrasidPoudel <prasidpoudel7.com>
2024-09-27 17:54:43 +0545

    Added a reset button and increased round to 10

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let computerScore=document.querySelector('#computer-score');
 const comp_icon=document.querySelector('.computer-icon')
 const human_icon=document.querySelector('.human-icon')
 const result=document.querySelector('.text-2')
+const cont=document.querySelector('.content')
 const winner=document.querySelector('.text-1')
 const choice=['rock','paper','scissors']
 const rock=document.getElementById('rock');
@@ -27,28 +28,25 @@ function playround(playerchoice) {
    computerScore.textContent=`Computer:${computerscore}`
    result.textContent=`You lost! ${computer_choice} beats ${playerchoice}`;
   }
-  if(humanscore+computerscore+tiescore===5 && humanscore>computerscore) {
+  if(humanscore+computerscore+tiescore===10 && humanscore>computerscore) {
     winner.textContent='You have won the game';
-    result.textContent='Please refresh the page to play again';
   }
- else if(humanscore+computerscore+tiescore===5 && humanscore<computerscore) {
-    winner.textContent='You have lost the game';
-    result.textContent='Please refresh the page to play again';
-    
+ else if(humanscore+computerscore+tiescore===10 && humanscore<computerscore) {
+    winner.textContent='You have lost the game';    
   }
-  else if(humanscore+computerscore+tiescore===5 && humanscore===computerscore) {
+  else if(humanscore+computerscore+tiescore===10 && humanscore===computerscore) {
     winner.textContent="It's a tie";
-    result.textContent='Please refresh the page to play again';
   }
   const all=document.querySelectorAll('.prasid');
-  if(humanscore+computerscore+tiescore>=5) {
-    all.forEach(box=> {
-      box.addEventListener('click',()=> {
-        alert('Refresh the page to play again');
+  const rest=document.createElement('button')
+  rest.classList.add('restart')
+    if(humanscore+computerscore+tiescore>=10) {
+      all.forEach((item)=> {
+        item.style.pointerEvents='none'
       })
-    })
-  
-  }
+      reset(rest,cont,all)
+    }
+    
 }
 function showemoji(player,computer) {
 
@@ -68,7 +66,17 @@ function showemoji(player,computer) {
             comp_icon.textContent = '✌️';
         }
     }
+    if(humanscore+computerscore+tiescore!==10) {
     rock.addEventListener('click', () => playround('rock'));
     paper.addEventListener('click', () => playround('paper'));
     scissors.addEventListener('click', () => playround('scissors'));
-  
+    }
+  function reset(child,parent,dis) {
+    child.setAttribute("style", "margin-left: 45%; margin-top: 15px; font-size: 25px; height: 60px; width: 90px; color: white; background-color: blue;");
+    child.textContent = 'Reset';    
+    parent.appendChild(child);
+    child.addEventListener('click',()=> {
+      window.location.reload()
+    })
+    }
+    
